a169eddcfb35c1e7f1a1d1701c3e66faf19f1e53
Horváth Gellért <gellerthorvath0@gmail.com>
2019-12-13 10:41:12 +0100

    adatbázis bool megírása

--- TobbformosMvcPizzaTobbTabla/TobbformosMvcPizzaTobbTabla/repository/Order/RepositoryDatabaseTableOrderSQL.cs
+++ TobbformosMvcPizzaTobbTabla/TobbformosMvcPizzaTobbTabla/repository/Order/RepositoryDatabaseTableOrderSQL.cs
@@ -45,7 +45,15 @@ namespace TobbbformosPizzaAlkalmazasTobbTabla.Repository
                                 if (goodResult)
                                 {
 
-                                    Order o = new Order(razon, vazon, fazon, datum, ido);
+                                    bool teljesitve = false;
+                                    int teljesitveInt;
+                                    goodResult = int.TryParse(dr["teljesites"].ToString(), out teljesitveInt);
+
+                                    if (teljesitveInt == 1)
+                                    {
+                                        teljesitve = true;
+                                    }
+                                    Order o = new Order(razon, vazon, fazon, datum, ido,teljesitve);
                                     orders.Add(o);
                                 }
                             }
